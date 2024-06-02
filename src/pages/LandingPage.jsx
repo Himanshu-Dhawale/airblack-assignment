3ff89423b5144b405c0872dbc52647b2b9dc7668
Himanshu <himanshudhawale9@gmail.com>
2024-06-02 16:33:38 +0530

    added icons

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,87 +1,11 @@
-// import React from 'react'
-
-// const LandingPage = () => {
-//   return (
-//     <div className='md:w-full'>
-//         <img src="src\assets\Image.png" alt="Image" className='w-[375px] h-[124px] bg-[#000000] md:w-full'/>
-//         <div className='w-[375px] min-h-[670.4px] h-auto pt-5 pr-6 pb-[30px] pl-6 flex gap-6 bg-[#0F1014]'>
-//           <div className='w-[327px] min-h-[165.4px] h-auto flex flex-col gap-[7.09px]'>
-//              <h1 className='w-[327px] h-[60px] text-white font-normal text-3xl leading-[30px]'>Professional Online Makeup Course</h1>
-//              <div className='w-[327px] min-h-[24.22px] flex gap-[10px]'>
-//               <div className='w-[166px] min-h-[24.22px] rounded-[5px] pw-[166.16px] h-[24.22px] p-[5.11px_11.08px_5.11px_11.08px] gap-[7px] rounded-tl-[5px] relative opacity-0 border-[2px] border-white'>
-//                  <img src="src/assets/Vector.png" alt="Vector" className='w-[8.66px] h-[11.9px] absolute top-[0.06px] left-[0.21px]  bg-[#fafafa]'/>
-//                  <p className='w-[128px] h-[13px] font-normal text-xs leading-[12.36px] text-[#fafafa]'>Certification Programme</p>
-//               </div>
-//              </div>
-             
-//           </div>
-          
-//         </div>
-//         <div className="w-[375px] min-h-[296.14px] pt-[30px] pr-[24px] pb-[30px] pl-[24px] flex flex-col bg-[url('src/assets/SecondImage.png')] relative items-center bg-[#050505]">
-//   {/* <img 
-//     src="src/assets/SecondImage.png" 
-//     alt="Second-Image" 
-//     className="w-[360px] h-[360px]"
-//   /> */}
-//   <div className="w-[327px] h-[60px] flex gap-[25px] text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-//     <img 
-//       src="src/assets/gradient-1.png" 
-//       alt="gradient-vector" 
-//       className="w-[44.5px] h-[5.62px]"
-//     />
-//     <div className=' w-[188px] m-auto'>
-//     <h1 className="font-normal text-2xl  leading-[30px] text-[#f5f5f5]">
-//       Why Should You Join Airblack?
-//     </h1>
-//     </div>
-//     <img 
-//       src="src/assets/gradient-2.png" 
-//       alt="gradient-vector" 
-//       className="w-[44.5px] h-[5.62px]"
-//     />
-//   </div>
-//   <div className='w-[327px] h-[76.14px] flex gap-[11px]'>
-//     <div className='w-[104px] h-[76.14px] flex flex-col items-center gap-2 text-center border'>
-//       <img src="src\assets\Frame1.png" alt="Frame" className='w-[34.14px] h-[34.14px]'/>
-//       <h2 className='w-[104px] h-[34px] font-normal text-sm leading-[17.05px] text-white'>Do-it-together, live on zoom</h2>
-//     </div>
-//     <div className='w-[104px] h-[76.14px] flex flex-col items-center gap-2 text-center border'>
-//       <img src="src\assets\Frame1.png" alt="Frame" className='w-[34.14px] h-[34.14px]'/>
-//       <h2 className='w-[104px] h-[34px] font-normal text-sm leading-[17.05px] text-white'>Do-it-together, live on zoom</h2>
-//     </div>
-//     <div className='w-[104px] h-[76.14px] flex flex-col items-center gap-2 text-center border'>
-//       <img src="src\assets\Frame1.png" alt="Frame" className='w-[34.14px] h-[34.14px]'/>
-//       <h2 className='w-[104px] h-[34px] font-normal text-sm leading-[17.05px] text-white'>Do-it-together, live on zoom</h2>
-//     </div>
-//   </div>
-// </div>
-//    <div className='w-[375px] h-[416px] py-[30px] px-[24px] flex flex-col gap-[14px] bg-[#000000]'>
-//      <div className='w-[327px] h-[90px] flex gap-[25px] items-center'>
-//       <img src="src\assets\gradient-1.png" alt="gradient" className='w-[31.5px] h-[5.62px]'/>
-//      <h1 className='w-[214px] h-[90px] font-normal text-2xl leading-[30px] text-[#f5f5f5]'>
-//   Get Certified From India's Biggest Beauty Platform
-// </h1>
-// <img src="src\assets\gradient-2.png" alt="gradient" className='w-[31.5px] h-[5.62px]'/>
-//      </div>
-//     <img src="src\assets\certification.png" alt="Certification" className='w-[361px] h-[252px]'/>
-//    </div>
-//    <div className='w-[375px] h-[332.8px] flex px-[260.5px] pt-[61.61px] pb-[62.4px] gap-6 bg[url("src/assets/FooterImage.png")]'>
-
-//    </div>
-//     </div>
-//   )
-// }
-
-// export default LandingPage
-
-
 import React from "react";
+
 
 const LandingPage = () => {
   return (
     <div className="w-full">
       <img
-        src="src\assets\Image.png"
+        src="Image.png"
         alt="bg-img"
         className=" h-[124px] bg-[#000000] w-full"
       />
@@ -93,25 +17,25 @@ const LandingPage = () => {
         </div>
         <div className="w-[327px] h-[24.22px] flex gap-[10px]">
           <div className="bg-[#FFFFFF24] w-full h-[24.22px] items-center justify-center flex gap-[7px]">
-          <img src="src\assets\SVG.png" alt="vector" className="w-[9px] h-[12.14px]"/>
+          <img src="SVG.png" alt="vector" className="w-[9px] h-[12.14px]"/>
           <p className=" h-[13px] font-normal text-xs leading-[12.36px] text-white">Certification Programme</p>
           </div>
           <div className="flex items-center w-full">
-          <img src="src\assets\framer.png" alt="framer" className="w-[9px] h-[12.14px]"/>
+          <img src="framer.png" alt="framer" className="w-[9px] h-[12.14px]"/>
           <p className=" h-[13px] font-normal text-xs leading-[12.36px] text-[#F7C852]">Rated 4.85/5</p>
           </div>
         </div>
         <div className="flex flex-col gap-2">
             <div className="flex gap-2">
-              <img src="src\assets\tick.png" alt="check" />
+              <img src="tick.png" alt="check" />
               <p className="text-white text-sm leading-[16.8px]">India’s No.1 Online Makeup Course</p>
             </div>
             <div className="flex gap-2">
-              <img src="src\assets\tick.png" alt="check" />
+              <img src="tick.png" alt="check" />
               <p className="text-white text-sm leading-[16.8px]">Learn by LIVE Do-it-Together Classes</p>
             </div>
             <div className="flex gap-2">
-              <img src="src\assets\tick.png" alt="check" />
+              <img src="tick.png" alt="check" />
               <p className="text-white text-sm leading-[16.8px]">Unlimited Practise Session to master skills</p>
             </div>
           </div>
@@ -227,7 +151,7 @@ const LandingPage = () => {
         <div className="w-[327px] h-[76.14px] flex gap-[11px] absolute bottom-24">
           <div className="w-[104px] h-[76.14px] flex flex-col items-center gap-2 text-center">
             <img
-              src="src\assets\Frame1.png"
+              src="Frame1.png"
               alt="Frame"
               className="w-[34.14px] h-[34.14px]"
             />
@@ -237,7 +161,7 @@ const LandingPage = () => {
           </div>
           <div className="w-[104px] h-[76.14px] flex flex-col items-center gap-2 text-center">
             <img
-              src="src\assets\Frame1.png"
+              src="Frame1.png"
               alt="Frame"
               className="w-[34.14px] h-[34.14px]"
             />
@@ -247,7 +171,7 @@ const LandingPage = () => {
           </div>
           <div className="w-[104px] h-[76.14px] flex flex-col items-center gap-2 text-center">
             <img
-              src="src\assets\Frame1.png"
+              src="Frame1.png"
               alt="Frame"
               className="w-[34.14px] h-[34.14px]"
             />
@@ -261,13 +185,13 @@ const LandingPage = () => {
       </div>
       <div className='py-[30px] px-[24px] flex flex-col gap-[14px] bg-[#000000]'>
      <div className='w-[327px] h-[90px] !ml-4 flex gap-[25px] items-center'>
-      <img src="src\assets\gradient-1.png" alt="gradient" className='w-[31.5px] h-[5.62px]'/>
+      <img src="gradient-1.png" alt="gradient" className='w-[31.5px] h-[5.62px]'/>
      <h1 className='w-[214px] h-[90px] font-normal text-center text-2xl leading-[30px] text-[#f5f5f5]'>
   Get Certified From India's Biggest Beauty Platform
 </h1>
-<img src="src\assets\gradient-2.png" alt="gradient" className='w-[31.5px] h-[5.62px]'/>
+<img src="gradient-2.png" alt="gradient" className='w-[31.5px] h-[5.62px]'/>
      </div>
-    <img src="src\assets\certification.png" alt="Certification" className='w-[361px] h-[252px]'/>
+    <img src="certification.png" alt="Certification" className='w-[361px] h-[252px]'/>
    </div>
    <div className=" bg-[url('src/assets/FooterImage.png')] min-h-[296.14px] pt-[30px] pr-[24px] pb-[30px] pl-[24px] flex flex-col items-center bg-[#050505] relative">
        <h1 className=" text-white font-normal text-2xl text-center leading-[30px]">Join our growing 
@@ -275,10 +199,10 @@ community of
 35,000+ alumni</h1>
 <button className="w-[296px] h-9 apply-btn absolute top-28">Apply Now</button>
 <div className="flex justify-center items-center gap-[17.35px] w-[186px] absolute bottom-14">
- <img src="src\assets\Instagram.png" alt="Instagram" className="w-[22.15px] h-[22.15px]" />
- <img src="src\assets\Facebook.png" alt="Facebook" className="w-[22.15px] h-[22.15px]" />
- <img src="src\assets\Linkedin.png" alt="LinkedIn" className="w-[22.15px] h-[22.15px]" />
- <img src="src\assets\twitter.png" alt="Twitter" className="w-[22.15px] h-[22.15px]" />
+ <img src="Instagram.png" alt="Instagram" className="w-[22.15px] h-[22.15px]" />
+ <img src="facebook.png" alt="Facebook" className="w-[22.15px] h-[22.15px]" />
+ <img src="Linkedin.png" alt="LinkedIn" className="w-[22.15px] h-[22.15px]" />
+ <img src="twitter.png" alt="Twitter" className="w-[22.15px] h-[22.15px]" />
 </div>
 
    </div>
